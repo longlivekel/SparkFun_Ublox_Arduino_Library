37efd354f4c9abdbc829e39e29fefd17b9981907
longlivekel <59575901+longlivekel@users.noreply.github.com>
2020-12-21 16:20:36 -0600

    Update Example5_SpeedHeadingPrecision.ino

--- examples/Example5_SpeedHeadingPrecision/Example5_SpeedHeadingPrecision.ino
+++ examples/Example5_SpeedHeadingPrecision/Example5_SpeedHeadingPrecision.ino
@@ -5,21 +5,16 @@
   Date: January 3rd, 2019
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
-
   This example shows how to query a Ublox module for its lat/long/altitude.
-
   Note: Long/lat are large numbers because they are * 10^7. To convert lat/long
   to something google maps understands simply divide the numbers by 1,000,000. We
   do this so that we don't have to use floating point numbers.
-
   Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
-
   Feel like supporting open source hardware?
   Buy a board from SparkFun!
   ZED-F9P RTK2: https://www.sparkfun.com/products/15136
   NEO-M8P RTK: https://www.sparkfun.com/products/15005
   SAM-M8Q: https://www.sparkfun.com/products/15106
-
   Hardware Connections:
   Plug a Qwiic cable into the GPS and a BlackBoard
   If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
@@ -56,13 +51,6 @@ void loop()
   {
     lastTime = millis(); //Update the timer
     
-    long latitude = myGPS.getLatitude();
-    Serial.print(F("Lat: "));
-    Serial.print(latitude);
-
-    long longitude = myGPS.getLongitude();
-    Serial.print(F(" Long: "));
-    Serial.print(longitude);
 
     long speed = myGPS.getGroundSpeed();
     long speedMPH = (speed * 0.00223694);
@@ -70,14 +58,7 @@ void loop()
     Serial.print(speedMPH);
     Serial.print(F(" (mph)"));
 
-    long heading = myGPS.getHeading();
-    Serial.print(F(" Heading: "));
-    Serial.print(heading);
-    Serial.print(F(" (degrees * 10^-5)"));
-
-    int pDOP = myGPS.getPDOP();
-    Serial.print(F(" pDOP: "));
-    Serial.print(pDOP / 100.0, 2);
+ 
 
     Serial.println();
   }
