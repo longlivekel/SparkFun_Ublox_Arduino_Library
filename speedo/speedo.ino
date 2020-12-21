04797efdde7f8654b38ababe3e1f66019680a9db
Ian Jennings <ian@futurehat.com>
2020-12-21 17:09:01 -0600

    Update speedo.ino

--- speedo/speedo.ino
+++ speedo/speedo.ino
@@ -1,31 +1,3 @@
-/*
-  Get Speed/Heading and dilution of precision via UBX binary commands - no more NMEA parsing!
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: January 3rd, 2019
-  License: MIT. See license file for more information but you can
-  basically do whatever you want with this code.
-
-  This example shows how to query a Ublox module for its lat/long/altitude.
-
-  Note: Long/lat are large numbers because they are * 10^7. To convert lat/long
-  to something google maps understands simply divide the numbers by 1,000,000. We
-  do this so that we don't have to use floating point numbers.
-
-  Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun!
-  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
-  NEO-M8P RTK: https://www.sparkfun.com/products/15005
-  SAM-M8Q: https://www.sparkfun.com/products/15106
-
-  Hardware Connections:
-  Plug a Qwiic cable into the GPS and a BlackBoard
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
-*/
-
 #include <Wire.h> //Needed for I2C to GPS
 
 #include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
@@ -37,7 +9,7 @@ void setup()
 {
   Serial.begin(115200);
   while (!Serial); //Wait for user to open terminal
-  Serial.println("SparkFun Ublox Example");
+  Serial.println("Kel's Speedometer");
 
   Wire.begin();
 
@@ -48,6 +20,8 @@ void setup()
   }
 }
 
+long distance = 0;
+
 void loop()
 {
   //Query module only every second. Doing it more often will just cause I2C traffic.
@@ -56,28 +30,18 @@ void loop()
   {
     lastTime = millis(); //Update the timer
     
-    long latitude = myGPS.getLatitude();
-    Serial.print(F("Lat: "));
-    Serial.print(latitude);
-
-    long longitude = myGPS.getLongitude();
-    Serial.print(F(" Long: "));
-    Serial.print(longitude);
-
     long speed = myGPS.getGroundSpeed();
     long speedMPH = (speed * 0.00223694);
     Serial.print(F(" Speed: "));
     Serial.print(speedMPH);
     Serial.print(F(" (mph)"));
-
-    long heading = myGPS.getHeading();
-    Serial.print(F(" Heading: "));
-    Serial.print(heading);
-    Serial.print(F(" (degrees * 10^-5)"));
-
-    int pDOP = myGPS.getPDOP();
-    Serial.print(F(" pDOP: "));
-    Serial.print(pDOP / 100.0, 2);
+    
+    distance = distance + (speed / 3600); // distance is equal to the old distance plus the new speed/seconds in an hour
+    
+    if (distance % .1 == 0) { // should happen every 10th of a mile
+      Serial.print(F(" Odometer: "));
+      Serial.print(distance);
+    }
 
     Serial.println();
   }
