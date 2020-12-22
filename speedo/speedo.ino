b29ade46f972b3e9a456589c07bc628bd57dd16f
Ian Jennings <ijenning@berkadia.com>
2020-12-21 21:34:39 -0600

    whoops. all better

--- speedo/speedo.ino
+++ speedo/speedo.ino
@@ -1,5 +1,7 @@
 #include <Wire.h> //Needed for I2C to GPS
 
+#include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
+SFE_UBLOX_GPS myGPS;
 
 long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to Ublox module.
 
@@ -8,6 +10,14 @@ void setup()
   Serial.begin(115200);
   while (!Serial); //Wait for user to open terminal
   Serial.println("Kel's Speedometer");
+
+  Wire.begin();
+
+  if (myGPS.begin() == false) //Connect to the Ublox module using Wire port
+  {
+    Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
 }
 
 float distance = 0;
@@ -22,14 +32,9 @@ void loop()
   {
     lastTime = millis(); //Update the timer
     
-    long speed = random(0, 22);
-    //float speedMPH = (speed * 0.00223694);
-    float speedMPH = 60;
+    long speed = random(min, max)
+    float speedMPH = (speed * 0.00223694);
     distance = distance + (speedMPH / 3600); // distance is equal to the old distance plus the new speed/seconds in an hour
-    
-    //if (distance >= (odo + .1)) { // should happen every 10th of a mile
-     // odo = distance;      
-    //}
     
     Serial.print(F(" Speed: "));
     Serial.print(speedMPH);
@@ -37,12 +42,13 @@ void loop()
     Serial.print(F(" Distance: "));
     Serial.print(distance, 6);
     Serial.print(F(" Miles "));
+    // every 10th of a mile
     if (distance >= (odo + .1)) {
       odo = distance;    
       Serial.print(F(" | Odometer: "));
       Serial.print(odo, 1);
     }
-    // if distance > previous odo + .01
+
     
     
 
